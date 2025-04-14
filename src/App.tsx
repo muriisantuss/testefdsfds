f868d2d097036bd2e577857083e173555d6fe32a
muriisantuss <muriisantuss@gmail.com>
2025-04-14 18:34:09 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { use, useState } from 'react';
+import './App.css';
+
+type SquareProps = {
+  value: string;
+  onSquareClick: () => void;
+};
+
+function Square({ value, onSquareClick }: SquareProps) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(''));
+
+  function handleClick() {
+    // setSquares('X');
+    alert('clicked!');
+  }
+
+  return (
+    <>
+      <div className="border-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
+      </div>
+      <div className="border-row">
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
+      </div>
+      <div className="border-row">
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
+      </div>
+    </>
+  );
+}
